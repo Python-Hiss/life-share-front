35b13d86d8426a6300522092fef9ed4d5b221ca8
Yahialabeeb <yahia.labeeb@gmail.com>
2022-01-01 19:38:36 +0200

    signup

--- pages/home_compnenet/Homa_page.js
+++ pages/home_compnenet/Homa_page.js
@@ -3,13 +3,15 @@ import Navbar from './Navbar'
 import Sectiontwo from './Sectiontwo'
 import LoginForm from './LoginForm'
 import Signupform from './Signupform'
+import Signupformhospital from './Signupfromhospital'
 const  Homa_page =()=> {
     return (
         <>
             {/* <Navbar/> */}
             {/* <Sectiontwo/> */}
-            <LoginForm/>
+            {/* <LoginForm/> */}
             {/* <Signupform/> */}
+            <Signupformhospital/>
         </>
     )
 }
