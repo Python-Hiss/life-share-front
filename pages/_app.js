43b41377994d860cbfb695da73f04df64361b03f
hayaa1234 <hayalawansah@gmail.com>
2022-01-03 16:01:48 +0200

    added some style

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import '../styles/video.css'
 import '../styles/tailwind.css'
 import '../styles/form.css'
-
+import '../styles/hospitalProfile.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
