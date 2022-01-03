dd6bf3da8d29c3e3dec1e377fec23beb5f93c587
yaseen1998 <yasseenkouthe@eng.hu.edu>
2022-01-03 20:08:48 +0200

    done

--- pages/Hospital_component/Table2.js
+++ pages/Hospital_component/Table2.js
@@ -9,15 +9,26 @@ const people = [
       image:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
     },
+    {
+      name: 'Jane Cooper',
+      title: 'Regional Paradigm Technician',
+      department: 'Optimization',
+      role: 'Admin',
+      email: 'jane.cooper@example.com',
+      image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
     // More people...
   ]
   
-  export default function Table() {
+  export default function Table(props) {
     return (
       <div className="flex flex-col w-4/5 m-auto mt-5">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+
+              
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
                   <tr>
@@ -52,8 +63,9 @@ const people = [
                     </th>
                   </tr>
                 </thead>
+                
                 <tbody className="bg-white divide-y divide-gray-200">
-                  {people.map((person) => (
+                  {props.data.map((person) => (
                     <tr key={person.email}>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
@@ -61,21 +73,21 @@ const people = [
                             <img className="w-10 h-10 rounded-full" src={person.image} alt="" />
                           </div>
                           <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">{person.name}</div>
+                            <div className="text-sm font-medium text-gray-900">{person.first_name}</div>
                             <div className="text-sm text-gray-500">{person.email}</div>
                           </div>
                         </div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">{person.title}</div>
-                        <div className="text-sm text-gray-500">{person.department}</div>
+                        <div className="text-sm text-gray-900">{person.address.city.city}</div>
+                        <div className="text-sm text-gray-500">{person.address.area.area}</div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <span className="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
-                          Active
+                          {person.blood_type.blood_type}
                         </span>
                       </td>
-                      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{person.role}</td>
+                      <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{person.phone_number}</td>
                 
                     </tr>
                   ))}
--- pages/Hospital_component/hospitalProfile.js
+++ pages/Hospital_component/hospitalProfile.js
@@ -1,28 +1,30 @@
 import axios from "axios";
 import { useState } from "react";
-import { GlobeAltIcon ,SearchIcon } from "@heroicons/react/outline";
+import { GlobeAltIcon, SearchIcon } from "@heroicons/react/outline";
 import Form2 from "./Form2";
 import Table from "./Table2";
 
 function HospitalProfile() {
   const [result, setResult] = useState([]);
-  const [location, setLocation] = useState([]);
-  const [bloodType, setBloodType] = useState([]);
-
-
-  let locationHandler = (e) => {
-    setLocation(e.target.value)
-  };
-  let bloodTypeHandler = (e) => {
-    setBloodType(e.target.value)
-  };
+  const url = 'http://127.0.0.1:8000/'
   let submitHandler = async (e) => {
+    let data = e.target.blood_type.value
+    let city = e.target.city.value
+    let area = e.target.area.value
+    let urls = `${url}accounts/blood/${data}/`
     e.preventDefault();
-    await axios.get("http://127.0.0.1:8000/account/view/").then((data) => {
-      let newData = data.data
-      let filteredData = newData.filter(item =>item.blood_type == bloodType && item.location == location)
-      setResult (filteredData)
-    });
+    const blood_data = await axios.get(urls)
+      let filteredData = blood_data.data.filter(
+        (item) =>  item.address.city.city == city
+      );
+      if (area){
+        filteredData = filteredData.filter(
+          (item) =>  item.address.area.area == area
+        );
+      }
+      console.log(filteredData);
+      setResult(filteredData);
+    // });
   };
   return (
     <>
@@ -36,16 +38,89 @@ function HospitalProfile() {
       <h1 className="text-4xl text-center text-blue-900">Isteqlal</h1>
       <div>
         <div className="flex">
-          <GlobeAltIcon className="h-9 w-9 top-1 right-1 "/>
+          <GlobeAltIcon className="h-9 w-9 top-1 right-1 " />
           <a href="https://istiklalhospital.com/">istiqlal-hospital.com</a>
         </div>
       </div>
 
       <div id="hospital-form-table">
-      <Form2 locationHandler={locationHandler} bloodTypeHandler={bloodTypeHandler} submitHandler={submitHandler}/>
+        <div>
+          <div class="md:grid md:grid-cols-2 md:gap-6 w-4/5 m-auto">
+            <div class="mt-5 md:mt-0 md:col-span-2">
+              <form onSubmit={submitHandler}>
+                <div class="shadow overflow-hidden sm:rounded-md">
+                  <div class="px-4 py-5 bg-white sm:p-6">
+                    <div class="grid grid-cols-6 gap-6">
+                      <div class="col-span-6 sm:col-span-3">
+                      <label
+                        className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
+                        htmlFor="grid-password"
+                      >
+                        blood_type
+                      </label>
+                      <select
+                        id="cars"
+                        name="blood_type"
+                        className="w-full h-10 px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
+                      >
+                        <option value="A+">A+</option>
+                        <option value="A-">A-</option>
+                        <option value="B+">B+</option>
+                        <option value="B-">B-</option>
+                        <option value="AB+">AB+</option>
+                        <option value="AB-">AB-</option>
+                        <option value="O+">O+</option>
+                        <option value="O-">O-</option>
+                      </select>
+                      </div>
 
-      <Table/>
-       
+                      <div class="col-span-6 sm:col-span-3">
+                        <label
+                          for="last-name"
+                          class="block text-sm font-medium text-gray-700"
+                          
+                        >
+                          City
+                        </label>
+                        <input
+                          type="text"
+                          name="city"
+                          id="last-name"
+                          autocomplete="family-name"
+                          class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <div class="col-span-6 sm:col-span-3">
+                        <label
+                          for="last-name"
+                          class="block text-sm font-medium text-gray-700"
+                          
+                        >
+                          Area
+                        </label>
+                        <input
+                          placeholder="just for filter"
+                          type="text"
+                          name="area"
+                          id="area"
+                          autocomplete="family-name"
+                          class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                    <button type="submit">
+                       <SearchIcon class="inline-flex justify-center  border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 w-9 h-9" />
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <Table data={result}/>
       </div>
     </>
   );
